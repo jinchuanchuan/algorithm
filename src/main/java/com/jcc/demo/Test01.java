3e1bc2b54964f4668dcff816a6cdd0d1b3c0fb9b
jinchuanchuan <sdzzjcc@163.com>
2019-03-29 00:49:27 +0800

    增加选择排序和冒泡排序

---
+++ src/main/java/com/jcc/demo/Test01.java
@@ -0,0 +1,10 @@
+package com.jcc.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+    }
+}
--- src/main/java/com/jcc/sort/Test.java
+++ src/main/java/com/jcc/sort/Test.java
@@ -3,10 +3,13 @@ package com.jcc.sort;
 public class Test {
     public static void main(String[] args) {
         int[] arr = {3, 6, 8, 45, 1, 25, 89, 6};
-//选择排序
+//        选择排序
 //        SelectionSort.selectionSort(arr);
 //        冒泡排序
 //        BubbleSort.BubbleSort(arr);
+//        shell排序
+//        ShellSort.sort(arr);
+//        快速排序
         QuickSort.quickSort(arr, 0, 7);
     }
 }
